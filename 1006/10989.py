4b45bbfdeddf1d542864412469208242b95676a0
yenawee <yaena0319@naver.com>
2022-10-06 19:06:20 +0900

    1006

---
+++ 1006/10989.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.getsizeof(1))
+
+#c_arr = [0] * 10001
+#for _ in range(int(sys.stdin.readline())) :
+#	c_arr[int(sys.stdin.readline())] += 1
+#for i in range(10001):
+#	if c_arr[i] :
+#		for j in range(c_arr[i]):
+#			print(i)
+
+
