9fa18bc301bec91fb52d278dba0b694e6dd7ce55
yenawee <yaena0319@naver.com>
2022-10-21 21:23:37 +0900

    1021

--- 1021/2477.py
+++ 1021/2477.py
@@ -1,15 +1,22 @@
 m = int(input())
 
-h =[]
-w = []
+l = []
+for _ in range(6):
+	i,j = map(int, input().split())
+	l.append(j)
 
-for _ in range(3):
-	i, j = map(int, input().split())
-	if i == 3 or i == 4 : h.append(j)
-	else : w.append(j)
+i = 0
+max1 = 0
+max2 = 0
+for n in l :
+	if i % 2 == 0 :
+		if n > max1 : max1=n; max1_ix = i
+	elif i % 2 != 0:
+		if n > max2 : max2=n; max2_ix = i
+	i += 1
 
-max_sq = max(h) * max(w)
+small1 = abs(l[(max1_ix + 1) % 6] - l[max1_ix - 1])
+small2 = abs(l[(max2_ix + 1) % 6] - l[max2_ix - 1])
 
-w.pop(max(w))
-
+print((max1 * max2 - small1 * small2) * m)
 
---
+++ 1021/3053.py
@@ -0,0 +1,4 @@
+from math import pi
+
+r = int(input())
+print(pi*(r**2),2*(r**2), sep='\n')
