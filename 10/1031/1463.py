9d3e56463f5d0b276d70c982927416d48471ce85
yenawee <yaena0319@naver.com>
2022-10-31 18:53:39 +0900

    1031

--- 10/1031/1463.py
+++ 10/1031/1463.py
@@ -1,7 +1,12 @@
 N = int(input())
+dp = [0] * 1000001
 
-# 1. 3으로 나누기
-# 2. 2로 나누기
-# 3. 1 빼기
+#  dp[i] = i 까지 계산했을 때 연산의 최소값
 
-# 3 으로 나누기 => 1을 빼서 3으로
+for i in range(2, N + 1):
+    if i % 6 == 0 : dp[i] = min(dp[i - 1], dp[ i //3], dp[i//2]) + 1
+    elif i % 3 == 0 : dp[i] = min(dp[i - 1], dp[ i // 3]) + 1
+    elif i % 2 == 0 : dp[i] = min(dp[i - 1], dp[ i // 2]) + 1
+    else : dp[i] = dp[i - 1] + 1
+
+print(dp[N])
